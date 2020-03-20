24486c70b74750e4cfdae84c6ac5ebe16a541d2a
mohammad <mohammades6634@yahoo.com>
2020-03-20 23:09:13 +0330

    final humidity sensor in ESP

---
+++ sensor_rotobat.ino
@@ -0,0 +1,30 @@
+#define LED 5
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin(9600);
+pinMode(0,INPUT);
+
+ pinMode (0,INPUT);
+  pinMode(LED,OUTPUT );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+float i;
+int YL69;
+i= analogRead(0);
+YL69 = 100-(i * 100)/1023;
+
+Serial.print("SM amount = ");
+Serial.println(i);
+Serial.print(YL69);
+Serial.println("%");
+delay (500);
+if(YL69<=40){
+   digitalWrite(LED,HIGH);
+}else{
+   digitalWrite(LED,LOW);
+}
+}
